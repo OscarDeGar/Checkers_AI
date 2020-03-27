ee794fd2e72adeed3f3039c9eb0c1e9bc0a9a32f
Oscar De La Garza <odelagarza@olin.edu>
2020-03-27 11:56:51 -0400

    Adding GameScript Framework

---
+++ Checkers.py
@@ -0,0 +1,11 @@
+""""""""""""""""""""""""""""
+
+MP4: Checkers_AI
+
+By: Oscar De La Garza & Yehya Albakri
+
+""""""""""""""""""""""""""""
+
+import os, sys
+import pygame
+from pygame.locals import*
